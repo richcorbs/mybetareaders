03a8aebf2e7f4b24d74d0443bd11ad6b164af280
Rich Corbridge <rich.corbridge@gmail.com>
2012-12-14 23:50:37 +0530

    Work on puma deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 set :stages, %w(staging production)
 require 'capistrano/ext/multistage'
+require 'puma/capistrano'
 
 set :application, "mybetareaders"
 
@@ -39,27 +40,3 @@ end
 after 'deploy:create_symlink', :update_links
 after "deploy", "deploy:cleanup"
 
-namespace :deploy do
-  set :shared_children, shared_children << 'tmp/sockets'
-
-  desc "Start the application"
-  task :start, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec puma -b 'unix://#{shared_path}/sockets/puma.sock' -S #{shared_path}/sockets/puma.state --control 'unix://#{shared_path}/sockets/pumactl.sock' >> #{shared_path}/log/puma-#{stage}.log 2>&1 &", :pty => false
-  end
-
-  desc "Stop the application"
-  task :stop, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec pumactl -S #{shared_path}/sockets/puma.state stop"
-  end
-
-  desc "Restart the application"
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec pumactl -S #{shared_path}/sockets/puma.state restart"
-  end
-
-  desc "Status of the application"
-  task :status, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec pumactl -S #{shared_path}/sockets/puma.state stats"
-  end
-
-end
