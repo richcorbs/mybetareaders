189182bbffd06be82bc2ab0a7ba6e388cef3e1f7
Rich Corbridge <rich.corbridge@gmail.com>
2012-12-14 22:59:24 +0530

    Work on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,22 +44,22 @@ namespace :deploy do
 
   desc "Start the application"
   task :start, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec puma -b 'unix://tmp/puma.sock' -S /tmp/puma.state --control 'unix://tmp/pumactl.sock' >> #{shared_path}/log/puma-#{stage}.log 2>&1 &", :pty => false
+    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec puma -b 'unix://#{shared_path}/sockets/puma.sock' -S #{shared_path}/sockets/puma.state --control 'unix://#{shared_path}/sockets/pumactl.sock' >> #{shared_path}/log/puma-#{stage}.log 2>&1 &", :pty => false
   end
 
   desc "Stop the application"
   task :stop, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec pumactl -S /tmp/puma.state stop"
+    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec pumactl -S #{shared_path}/sockets/puma.state stop"
   end
 
   desc "Restart the application"
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec pumactl -S /tmp/puma.state restart"
+    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec pumactl -S #{shared_path}/sockets/puma.state restart"
   end
 
   desc "Status of the application"
   task :status, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec pumactl -S /tmp/puma.state stats"
+    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec pumactl -S #{shared_path}/sockets/puma.state stats"
   end
 
 end
