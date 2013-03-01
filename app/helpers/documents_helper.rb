4f8838e18f9e510955444473164b50fb0a49e0aa
Rich Corbridge <rich.corbridge@gmail.com>
2013-03-01 13:16:09 +0800

    More work on book-icon and ghosts...

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,2 +1,11 @@
 module DocumentsHelper
+  def book_icon_background_color document
+    if document.active? && document.book_icon_color
+      document.book_icon_color
+    elsif document.active?
+      'green'
+    else
+      '#f0f0f0'
+    end
+  end
 end
